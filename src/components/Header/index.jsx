b8dbbea9d52a76279358b6c2f5a6c82a4da8aa45
Rafael Moura <rafaelcostamoura@gmail.com>
2022-06-12 11:26:38 -0300

    Iniciando Projeto com Vite e Criando Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./header.module.css";
+
+function Header() {
+  return (
+    <div className={styles.header}>
+      <h1>Ignite Feed</h1>
+    </div>
+  );
+}
+
+export default Header;
